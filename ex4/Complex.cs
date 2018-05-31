ffbf6f1a94d3f814199850b77e4dddc2e67ac073
jancuz <jancuz@yandex.ru>
2018-05-31 17:32:01 +0500

    Создан класс Complex, перегружена операция сложения

---
+++ ex4/Complex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+
+namespace ex4
+{
+    class Complex //класс, отвечающий за выполения действий над комплексными числами
+    {
+        public double RealNumber;
+        public double ImaginaryNumber;
+
+        public Complex(double a, double b)
+        {
+            RealNumber = a;
+            ImaginaryNumber = b;
+        }
+
+        // перегрузка оператора +
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.RealNumber + b.RealNumber, a.ImaginaryNumber + b.ImaginaryNumber);
+        }
+        public static Complex operator +(double num, Complex a)
+        {
+            return new Complex(a.RealNumber + num, a.ImaginaryNumber);
+        }
+    }
+}
+
